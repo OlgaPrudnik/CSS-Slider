bcfb2e260db323bc49b363a1c30703df5c4c5de9
Olga <olga.barannikova.92@mail.ru>
2022-05-16 12:48:42 +0300

    feat: add script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-// let counter = 1;
-// setInterval(function(){
-//    document.getElementById('radio' + counter).checked = true;
-//    counter++;
-//    if(counter > 4){
-//        counter = 1;
-//    }
-// }, 5000)
+let counter = 1;
+setInterval(function(){
+   document.getElementById('radio' + counter).checked = true;
+   counter++;
+   if(counter > 4){
+       counter = 1;
+   }
+}, 5000)
